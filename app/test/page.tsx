4f54b61eb554c4b5655114bc9b8aebf69e0ce37f
CiurtinAndrei <149907852+CiurtinAndrei@users.noreply.github.com>
2025-03-12 22:16:34 +0200

    initial commit

---
+++ app/test/page.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function Test(){
+
+    
+
+    return(
+        <>
+        
+        Test page!
+       <h1><Link href={'/'}>Inapoi</Link></h1> 
+        </>
+    );
+
+
+}
